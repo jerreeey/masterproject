4b5e9c165db5f76e75f0a57f3c42bbb43a63a59b
Jeremias Rauwolf <82889934+jerreeey@users.noreply.github.com>
2024-02-03 16:04:48 +0100

    fix luminance formula, added handling for subpixel antialiasing

--- wp-content/plugins/imageContrastChecker/scripts/main.js
+++ wp-content/plugins/imageContrastChecker/scripts/main.js
@@ -1,81 +1,182 @@
-const {select,dispatch} = wp.data
-import {createCanvasFromImage, createOverlayCanvas, calculateContrast, isLargeText} from './imageContrastChecker.js'
+const { select, dispatch } = wp.data;
+import {
+  createCanvasFromImage,
+  createOverlayCanvas,
+  calculateContrast,
+  isLargeText,
+} from "./imageContrastChecker.js";
 
 const { registerPlugin } = wp.plugins;
 const { PluginPostStatusInfo } = wp.editPost;
 const { Button } = wp.components;
 
-const MyPlugin = function() {
-    return wp.element.createElement(
-        PluginPostStatusInfo,
-        {},
-        wp.element.createElement(
-            Button,
-            { isPrimary: true, onClick: checkBlocks },
-            'Check contrast'
-        )
-    );
+const MyPlugin = function () {
+  return wp.element.createElement(
+    PluginPostStatusInfo,
+    {},
+    wp.element.createElement(
+      Button,
+      { isPrimary: true, onClick: checkBlocks },
+      "Check contrast"
+    )
+  );
 };
-registerPlugin( 'my-plugin', { render: MyPlugin } );
-  
-wp.domReady(function(){
-    test()
+registerPlugin("my-plugin", { render: MyPlugin });
+
+wp.domReady(function () {
+  test();
 });
-function test() { 
-    const { isSavingPost } = wp.data.select( 'core/editor' );
-    let checked = true; // Start in a checked state.
-    wp.data.subscribe( () => {
-        if (isSavingPost()) {
-            checked = false;
-        } else {
-            if (!checked) {
-                checkBlocks()
-                checked = true;
-            }
-        }
-    });
+function test() {
+  const { isSavingPost } = wp.data.select("core/editor");
+  let checked = true; // Start in a checked state.
+  wp.data.subscribe(() => {
+    if (isSavingPost()) {
+      checked = false;
+    } else {
+      if (!checked) {
+        checkBlocks();
+        checked = true;
+      }
+    }
+  });
 }
 
-function checkBlocks() {
-    const blocks = wp.data.select('core/editor').getBlocks()
-    const editorSettings = wp.data.select("core/editor").getEditorSettings()
-    const { createWarningNotice } = wp.data.dispatch('core/notices');
+function createBlockNotice(blockClientId, result) {
+  const noticeId = `low-contrast-warning-${blockClientId}`;
+  console.log(result);
+  let message = ''
+  if (result.compliance === "AA") {
+    if (result.size === "large") {
+      message = `The text in the block ${blockClientId} does not comply with WCAG AA. The current contrast ratio is ${result.contrast.toFixed(2)}. For the current text size (more than or equal to 18pt or 14pt bold), the contrast ratio should be at least 3:1.`;
+    } else {
+      message = `The text in the block ${blockClientId} does not comply with WCAG AA. The contrast ratio is ${result.contrast.toFixed(2)}. For the current text size (less than 18pt or 14pt bold), the contrast ratio should be at least 4.5:1.`;
+    }
+  } else if (result.compliance === "AAA") {
+    if (result.size === "large") {
+      message = `The text in the block ${blockClientId} does not comply with WCAG AAA. The current contrast ratio is ${result.contrast.toFixed(2)}. For the current text size (more than or equal to 18pt or 14pt bold), the contrast ratio should be at least 4.5:1.`;
+    } else {
+      message = `The text in the block ${blockClientId} does not comply with WCAG AAA. The contrast ratio is ${result.contrast.toFixed(2)}. For the current text size (less than 18pt or 14pt bold), the contrast ratio should be at least 7:1.`;
+    }
+  }
 
-    console.log(editorSettings)
+  dispatch("core/notices").createNotice(
+    result.compliance === "AAA" ? "warning" : "error",
+    message,
+    {
+      id: noticeId,
+      isDismissible: true,
+      actions: [
+        {
+          url: `#block-${blockClientId}`,
+          label: "Go to Block",
+        },
+      ],
+    }
+  );
+}
 
-    // wp.data.dispatch( 'core/editor' ).updateEditorSettings( {colors: [{name: 'test', slug: 'test', color: '#00FF00'}]} );
-    blocks.forEach(async block => {
-        if(block.name === "core/cover" && block.attributes.id) {
-            // cover block with image detected
-            console.log(block)
+function createSuccessNotice() {
+  const noticeId = `low-contrast-success`;
 
-            // get dom element via "data-block" or "id" ('block-{clientId}')
-            const blockElement = document.querySelector(`[data-block="${block.clientId}"]`);
-            const blockElementBoundingRect = blockElement.getBoundingClientRect()
-            const img = blockElement.querySelector('img');
-            const overlayContainer = blockElement.querySelector('.wp-block-cover__inner-container')
-            const overlayText = overlayContainer.firstChild
-            const overlayTextBoundingRect = overlayText.getBoundingClientRect()
-            const position = {
-                top: overlayTextBoundingRect.top - blockElementBoundingRect.top,
-                left: overlayTextBoundingRect.left - blockElementBoundingRect.left,
-            };
-            try {
-                const imgData = await createCanvasFromImage(img, img.width, img.height)
-                const overlayData = createOverlayCanvas(overlayText, img.width, img.height, position)
-                calculateContrast(imgData, overlayData, isLargeText(overlayText))
+  dispatch("core/notices").createNotice(
+    "success",
+    `All blocks comply with the Web Content Accessibility Guidelines.`,
+    {
+      id: noticeId,
+      isDismissible: true,
+    }
+  );
+}
 
-                let contrast = 3
+function createWarningNotice(compliance) {
+  const noticeId = `low-contrast-warning`;
 
-                if (contrast < 4.5) { // Replace 4.5 with the minimum acceptable contrast
-                    createWarningNotice(`The contrast for block ${block.clientId} is too low.`, {
-                        id: `low-contrast-warning-${block.clientId}`,
-                        isDismissible: true,
-                    });
-                }
-            } catch (error) {
-                console.log(error)
-            }
+  dispatch("core/notices").createNotice(
+    compliance === "AAA" ? "warning" : "error",
+    `Some blocks do not comply with WCAG ${compliance}. Please check the blocks and change the appearence of the text or the image, by changing the position or size of the text or adding a text background. For more information to conforming to the Web Content Accessibility Guidelines click on the links below.`,
+    {
+      id: noticeId,
+      isDismissible: true,
+      actions: [
+        {
+          url: "https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html",
+          label: "More infomations to WCAG AA compliance",
+        },
+        {
+          url: "https://www.w3.org/WAI/WCAG21/Understanding/contrast-minimum.html",
+          label: "More infomations to WCAG AAA compliance",
+        },
+      ],
+    }
+  );
+}
+
+async function checkBlocks() {
+  const blocks = wp.data.select("core/editor").getBlocks();
+  const notices = wp.data.select("core/notices").getNotices();
+  const editorSettings = wp.data.select("core/editor").getEditorSettings();
+  let fail = false;
+  let leastCompliance = "";
+
+  notices.forEach((notice) => {
+    wp.data.dispatch("core/notices").removeNotice(notice.id);
+  });
+
+  await Promise.all(
+    blocks.map(async (block) => {
+      if (block.name === "core/cover" && block.attributes.id) {
+        const blockElement = document.querySelector(
+          `[data-block="${block.clientId}"]`
+        );
+        const blockElementBoundingRect = blockElement.getBoundingClientRect();
+        const img = blockElement.querySelector("img");
+        const overlayContainer = blockElement.querySelector(
+          ".wp-block-cover__inner-container"
+        );
+        const overlayText = overlayContainer.firstChild;
+        const overlayTextBoundingRect = overlayText.getBoundingClientRect();
+        const position = {
+          top: overlayTextBoundingRect.top - blockElementBoundingRect.top,
+          left: overlayTextBoundingRect.left - blockElementBoundingRect.left,
+        };
+        try {
+          const imgData = await createCanvasFromImage(
+            img,
+            img.width,
+            img.height
+          );
+          const overlayData = createOverlayCanvas(
+            overlayText,
+            img.width,
+            img.height,
+            position
+          );
+
+          // Get the text color and convert it to an array of color channels
+          const textColor = window.getComputedStyle(overlayText, null).getPropertyValue("color").replace(/[^\d,]/g, '').split(',')
+
+          const result = calculateContrast(
+            imgData,
+            overlayData,
+            isLargeText(overlayText),
+            textColor
+          );
+
+          if (result.fail) {
+            fail = true;
+            leastCompliance = leastCompliance == "AA" ? "AA" : result.compliance;
+            createBlockNotice(block.clientId, result);
+          }
+        } catch (error) {
+          console.log(error);
         }
+      }
     })
+  );
+
+  if (!fail) {
+    createSuccessNotice();
+  } else {
+    createWarningNotice(leastCompliance);
+  }
 }
